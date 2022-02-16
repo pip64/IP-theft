1e2f7c5b3e65f4f7e717d4316ad2117178f0f8b7
MixAM333 <76626915+MixAM333@users.noreply.github.com>
2022-02-16 21:21:44 +0700

    iptheft

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$fp = fopen("ips.txt", "a+");
+
+function getIp() {
+  $keys = [
+    'HTTP_CLIENT_IP',
+    'HTTP_X_FORWARDED_FOR',
+    'REMOTE_ADDR'
+  ];
+  foreach ($keys as $key) {
+    if (!empty($_SERVER[$key])) {
+      $ip = trim(end(explode(',', $_SERVER[$key])));
+      if (filter_var($ip, FILTER_VALIDATE_IP)) {
+        return $ip;
+      }
+    }
+  }
+}
+
+$ip = getIp();
+// выведем IP клиента на экран
+
+echo 'You are trolled!' . $ip;
+fwrite($fp, "IP: " . $ip . "\n" . "\n");
+fclose($fp);
+
+?>
