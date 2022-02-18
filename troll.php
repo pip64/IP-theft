9af3088f7659e209bf66ee0cf758a6b4fb5e8a3f
pip64 <76626915+pip64@users.noreply.github.com>
2022-02-18 18:36:50 +0700

    IPtheft Code

---
+++ troll.php
@@ -0,0 +1,29 @@
+<?php
+$fp = fopen("ips.txt", "a+");
+echo "<link rel='stylesheet' href='style.css'>";
+
+function getIp() {
+  $keys = [
+    'HTTP_CLIENT_IP',
+    'HTTP_X_FORWARDED_FOR',
+    'REMOTE_ADDR'
+  ];
+  foreach ($keys as $key) {
+    if (!empty($_SERVER[$key])) {
+      $ip = trim(end(explode(',', $_SERVER[$key])));
+      if (filter_var($ip, FILTER_VALIDATE_IP)) {
+        return $ip;
+      }
+    }
+  }
+}
+
+$ip = getIp();
+
+fwrite($fp, "IP: " . $ip . "\n" . "\n");
+fclose($fp);
+
+echo '<h1>But not me</h1>';
+echo '<h1><img src = "https://c.tenor.com/o656qFKDzeUAAAAC/rick-astley-never-gonna-give-you-up.gif"></h1>';
+?>
+
